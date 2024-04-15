476e9a676c3fe2beed61ce1a28ce6b9c0aef1c48
Bekmurodjon0904 <167044852+Bekmurodjon0904@users.noreply.github.com>
2024-04-15 15:09:53 +0500

    Update models.py
    Hozircha bilganimni yozib qo'ydim sizlarham qo'shimcha kiriting!

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+        USER_ROLES = [
+        ("translator", "Translator"),
+        ("client", "Client"),
+        ],
+        first_name = models.CharField(max_length=255, blank=True)
+        last_name = models.CharField(max_length=255, blank=True)
+        phone_number = models.IntegerField(max_length=15, blank=True)
+        type = models.CharField(max_length=255, choices=USER_ROLES)
+        ielts_rank = models.IntegerField(max_length=15, blank=True)
+        price = models.CharField(max_length=255, blank=True)
+        languages = models.CharField(max_length=255, blank=True)
+        template_text = models.CharField(max_length=255, blank=True)
